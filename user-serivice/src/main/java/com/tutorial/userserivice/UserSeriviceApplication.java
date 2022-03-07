fef88f9e3a95baf909edc16e33c5c0aa43acf839
Andres Toledo <atm1996.ast@gmail.com>
2022-03-07 13:46:44 -0600

    first commit

---
+++ user-serivice/src/main/java/com/tutorial/userserivice/UserSeriviceApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.userserivice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSeriviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSeriviceApplication.class, args);
+	}
+
+}
